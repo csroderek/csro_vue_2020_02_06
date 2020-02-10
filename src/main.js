fc438f9d3a4bc9e52db40b2b30142a22836cd44d
dereklitaoo <dereklitao@gmail.com>
2020-02-10 19:08:35 +0800

    ws ok

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import './plugins/axios'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import "./plugins/axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import VueNativeSock from "vue-native-websocket";
 
-Vue.config.productionTip = false
+Vue.use(VueNativeSock, "ws://192.168.2.2:8123/api/websocket", {
+  store: store,
+  format: "json"
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -9,5 +9,36 @@ export default new Vuex.Store({
   modules: {
     User,
     Home
+  },
+  state: {
+    socket: {
+      isConnected: false,
+      message: "",
+      reconnectError: false
+    }
+  },
+  mutations: {
+    SOCKET_ONOPEN(state, event) {
+      Vue.prototype.$socket = event.currentTarget;
+      state.socket.isConnected = true;
+      console.log("connected");
+    },
+    SOCKET_ONCLOSE(state, event) {
+      state.socket.isConnected = false;
+    },
+    SOCKET_ONERROR(state, event) {
+      console.error(state, event);
+    },
+    // default handler called for all methods
+    SOCKET_ONMESSAGE(state, message) {
+      state.socket.message = message;
+    },
+    // mutations for reconnect methods
+    SOCKET_RECONNECT(state, count) {
+      console.info(state, count);
+    },
+    SOCKET_RECONNECT_ERROR(state) {
+      state.socket.reconnectError = true;
+    }
   }
 });
