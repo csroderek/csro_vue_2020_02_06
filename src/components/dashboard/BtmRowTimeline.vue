f85031b5d7c5df19a4711f234fb5f94e6cbaed16
dereklitaoo <dereklitao@gmail.com>
2020-02-22 23:25:30 +0800

    add timeline

--- src/components/dashboard/BtmRowTimeline.vue
+++ src/components/dashboard/BtmRowTimeline.vue
@@ -1,16 +1,57 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items-per-page="10"
-    :items="desserts"
-    class="elevation-1"
-    :height="cardheight"
-    hide-default-footer
-  ></v-data-table>
-
-  <!-- <v-card :height="cardheight">
-
-  </v-card>-->
+  <v-card :height="cardheight" style="overflow:hidden">
+    <v-timeline class="overline" dense="">
+      <v-timeline-item small="" class="csro_timeline" color="purple lighten-2">
+        <v-row>21:30</v-row>
+        <v-row>
+          客厅吸顶灯
+          <v-icon small="" color="orange">mdi-weather-sunny</v-icon>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item small="">
+        <v-row>21:30</v-row>
+        <v-row>
+          客厅吸顶灯
+          <v-icon small="" color="orange">mdi-weather-sunny</v-icon>
+        </v-row></v-timeline-item
+      >
+      <v-timeline-item small="">
+        <v-row>21:30</v-row>
+        <v-row>
+          客厅吸顶灯
+          <v-icon small="" color="orange">mdi-weather-sunny</v-icon>
+        </v-row></v-timeline-item
+      >
+      <v-timeline-item small="" class="csro_timeline" color="purple lighten-2">
+        <v-row>21:30</v-row>
+        <v-row>
+          客厅吸顶灯
+          <v-icon small="" color="orange">mdi-weather-sunny</v-icon>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item small="" class="csro_timeline" color="purple lighten-2">
+        <v-row>21:30</v-row>
+        <v-row>
+          客厅吸顶灯
+          <v-icon small="" color="orange">mdi-weather-sunny</v-icon>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item small="" class="csro_timeline" color="purple lighten-2">
+        <v-row>21:30</v-row>
+        <v-row>
+          客厅吸顶灯
+          <v-icon small="" color="orange">mdi-weather-sunny</v-icon>
+        </v-row>
+      </v-timeline-item>
+      <v-timeline-item small="" class="csro_timeline" color="purple lighten-2">
+        <v-row>21:30</v-row>
+        <v-row>
+          客厅吸顶灯
+          <v-icon small="" color="orange">mdi-weather-sunny</v-icon>
+        </v-row>
+      </v-timeline-item>
+    </v-timeline>
+  </v-card>
 </template>
 
 <script>
@@ -18,144 +59,29 @@ import { mapState, mapGetters } from "vuex";
 export default {
   props: {},
   data() {
-    return {
-      headers: [
-        { text: "设备", align: "left", sortable: false, value: "calories" },
-        { text: "时间", align: "left", sortable: false, value: "fat" },
-        { text: "动作", align: "left", sortable: false, value: "carbs" }
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        }
-      ]
-    };
+    return {};
   },
   computed: {
     ...mapState({
-      cardheight: state => state.rowheight.btm,
-      lights: state => state.Global.lights,
-      covers: state => state.Global.covers,
-      climates: state => state.Global.climates,
-      sensors: state => state.Global.sensors,
-      scenes: state => state.Global.scenes
+      cardheight: state => state.rowheight.btm
     }),
-    ...mapGetters({
-      lights: "Global/lights",
-      covers: "Global/covers",
-      climates: "Global/climates",
-      sensors: "Global/sensors",
-      scenes: "Global/scenes"
-    }),
-
-    series() {
-      return [
-        this.lights.length,
-        this.covers.length,
-        this.climates.length,
-        this.sensors.length,
-        this.scenes.length
-      ];
-    }
+    ...mapGetters({})
   },
-  mounted() {
-    console.log(this.lights);
-  },
+  mounted() {},
   methods: {},
   beforeDestroy() {}
 };
 </script>
-<style scoped></style>
+
+<style scoped>
+.v-timeline-item__body {
+  min-width: 320px;
+}
+.csro_timeline {
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 16px;
+  position: relative;
+  overflow: hidden;
+}
+</style>
